3f9acc5c84981822987d0fa8b73f7565f5a455c2
TIRSA30 <93311704+TIRSA30@users.noreply.github.com>
2021-11-11 21:57:10 +0700

    3_A_71210700

---
+++ 3_A_71210700.py
@@ -0,0 +1,22 @@
+#Menambah catatan di program Doni
+
+#defenisi data test case 1
+Daftar1 = input("Masukan daftar belanja 1: ")
+Daftar2 = input("Masukan daftar belanja 2: ")
+Daftartambahan1 = input("Tambahan data ke daftar belanja 1: ")
+Daftartambahan2 = input("Tambahan data ke daftar belanja 2: ")
+
+#penyelesaian
+print("Daftar belanja 1 adalah", Daftar1 +","+Daftartambahan1)
+print("Daftar belanja 2 adalah", Daftar2 +","+Daftartambahan2)
+
+
+#defenisi data test case 2
+Daftar1 = input("Masukan daftar belanja 1: ")
+Daftar2 = input("Masukan daftar belanja 2: ")
+Daftartambahan1 = input("Tambahan data ke daftar belanja 1: ")
+Daftartambahan2 = input("Tambahan data ke daftar belanja 2: ")
+
+#penyelesaian
+print("Daftar belanja 1 adalah", Daftar1 +","+Daftartambahan1)
+print("Daftar belanja 2 adalah", Daftar2 +","+Daftartambahan2)
