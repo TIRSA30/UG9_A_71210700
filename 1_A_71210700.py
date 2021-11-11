3b4a90d6e72b94b0997099f8c8d37b74e16aa97d
TIRSA30 <93311704+TIRSA30@users.noreply.github.com>
2021-11-11 21:01:53 +0700

    1_A_71210700

---
+++ 1_A_71210700.py
@@ -0,0 +1,21 @@
+#Menghitung keliling lingkaran
+#diameter, phi, jari-jari, keliling
+#d, p, r, k
+#rumus jari-jari lingkaran  = 0.5*diameter
+#rumus keliling lingkaran = diameter*phi
+
+#defenisi data test case 1
+diameter = int(input("masukan diameter :"))
+phi = 3.14
+
+#proses
+keliling = diameter*phi
+print("keliling lingkaran adalah : ",round(keliling,2))
+
+#defenisi data test case 2
+diameter =int(input("masukan diameter :"))
+phi =  3.14
+
+#proses
+keliling = diameter*phi
+print("keliling lingkaran adalah : ",round(keliling,2))
