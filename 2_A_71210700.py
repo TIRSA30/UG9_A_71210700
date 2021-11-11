aa2a5006127422488a45a7ad26ec631a7076a098
TIRSA30 <93311704+TIRSA30@users.noreply.github.com>
2021-11-11 21:20:29 +0700

    2_A_71210700

---
+++ 2_A_71210700.py
@@ -0,0 +1,18 @@
+#Menghitung perpangkatan 5
+
+#defenisi data test case 1
+angka1 = int(input("Masukan angka: "))
+BesarNilaiPangkat = 5
+
+#proses
+Hasil = pow(angka1, BesarNilaiPangkat)
+print(angka1,"pangkat",BesarNilaiPangkat,"adalah",Hasil)
+
+
+#defenisi data test case 2
+angka1 = int(input("Masukan angka: "))
+BesarNilaiPangkat = 2
+
+#proses
+Hasil = pow(angka1, BesarNilaiPangkat)
+print(angka1,"pangkat",BesarNilaiPangkat,"adalah",Hasil)
